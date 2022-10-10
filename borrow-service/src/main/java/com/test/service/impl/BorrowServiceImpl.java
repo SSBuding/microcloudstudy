862a2c1772074d7e444250e2f63d7c2a81d3a7c8
SSbuding <2771576733@qq.com>
2022-10-10 21:45:14 +0800

    add eureka

--- borrow-service/src/main/java/com/test/service/impl/BorrowServiceImpl.java
+++ borrow-service/src/main/java/com/test/service/impl/BorrowServiceImpl.java
@@ -19,19 +19,22 @@ public class BorrowServiceImpl implements BorrowService {
     @Resource
     BorrowMapper mapper;
 
+    @Resource
+    RestTemplate template;
+
     @Override
     public UserBorrowDetail getUserBorrowDetailByUid(int uid) {
         List<Borrow> borrow = mapper.getBorrowsByUid(uid);
         //那么问题来了，现在拿到借阅关联信息了，怎么调用其他服务获取信息呢？
         //RestTemplate支持多种方式的远程调用
-        RestTemplate template = new RestTemplate();
+
         //这里通过调用getForObject来请求其他服务，并将结果自动进行封装
         //获取User信息
-        User user = template.getForObject("http://localhost:8101/user/"+uid, User.class);
+        User user = template.getForObject("http://userservice/user/"+uid, User.class);
         //获取每一本书的详细信息
         List<Book> bookList = borrow
                 .stream()
-                .map(b -> template.getForObject("http://localhost:8201/book/"+b.getBid(), Book.class))
+                .map(b -> template.getForObject("http://bookservice/book/"+b.getBid(), Book.class))
                 .collect(Collectors.toList());
         return new UserBorrowDetail(user, bookList);
     }
