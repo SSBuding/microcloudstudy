8313d31af31a776a22c4e9986c482a32cc98e3a2
SSbuding <2771576733@qq.com>
2022-10-10 14:14:20 +0800

    Initial project

---
+++ borrow-service/src/main/java/com/test/service/BorrowService.java
@@ -0,0 +1,7 @@
+package com.test.service;
+
+import com.test.entity.UserBorrowDetail;
+
+public interface BorrowService {
+    UserBorrowDetail getUserBorrowDetailByUid(int uid);
+}
---
+++ commons/src/main/java/com/test/entity/User.java
@@ -0,0 +1,11 @@
+package com.test.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    int uid;
+    String name;
+    int age;
+    String sex;
+}
